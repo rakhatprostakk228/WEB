b696fe5bc80d71d2ed2b0bd8355559c8661964a9
Rakhimbekov Rakhat <rkhantemir@mail.ru>
2024-11-06 13:46:15 +0500

    practice lesson 12

---
+++ practice12/practice_lesson_12/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Practice lesson 12</h1>
+        <h4>Framework is a tool that provides ready-made components or solutions that are customized to speed up development.</h4>
+        <h4>Using a framework offers advantages like increased productivity, better code organization for maintainability, and built-in security features.</h4>
+        <h4>I choose React, because I have worked with this framework and I'm eager to work with that in my future career.</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
